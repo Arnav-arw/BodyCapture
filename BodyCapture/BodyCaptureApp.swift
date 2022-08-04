0e900eed023361cbee6bc6562aca556cec1bd2dd
Arnav Singhal <singhal.arnav@gmail.com>
2022-08-04 10:36:05 +0530

    Initial Commit

---
+++ BodyCapture/BodyCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyCaptureApp.swift
+//  BodyCapture
+//
+//  Created by Arnav Singhal on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct BodyCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BodyCapture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BodyCapture
+//
+//  Created by Arnav Singhal on 04/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
